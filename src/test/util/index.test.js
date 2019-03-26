d504b81a7aec7fc5a533e68563b718ca4ae4f6ac
Jaramillo, Francisco <pacho@live.ca>
2019-03-26 13:05:24 -0400

    util tests

---
+++ src/test/util/index.test.js
@@ -0,0 +1,37 @@
+import { preventDefault, setAuthToken } from '../../util'
+import axios from 'axios'
+
+describe('Util functions Test', () => {
+  describe('preventDefault', () => {
+    it('should wrap the passed function and prevent event default', () => {
+      const callOrder = []
+
+      const mockEvent = {
+        preventDefault: jest.fn(() => callOrder.push('preventDefault'))
+      }
+
+      const eventHandler = jest.fn(() => callOrder.push('eventHandler'))
+      const wrappedEventHandler = preventDefault(eventHandler)
+
+      wrappedEventHandler(mockEvent)
+
+      expect(mockEvent.preventDefault).toHaveBeenCalledTimes(1)
+      expect(eventHandler).toHaveBeenCalledTimes(1)
+      expect(callOrder).toEqual(['preventDefault', 'eventHandler'])
+    })
+  })
+
+  describe('setAuthToken', () => {
+    jest.mock('axios')
+    it("should set axios' Authorization header if a token is given", () => {
+      const superToken = 'elSuperAuthToken'
+      setAuthToken(superToken)
+      expect(axios.defaults.headers.common['Authorization']).toEqual(superToken)
+    })
+
+    it("should unset axios' Authorization header if token is null ", () => {
+      setAuthToken()
+      expect(axios.defaults.headers.common['Authorization']).toBeUndefined()
+    })
+  })
+})
